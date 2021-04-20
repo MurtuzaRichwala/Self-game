ba3483a757b48efe399e0ae775674abb225e804a
MurtuzaRichwala <74816569+MurtuzaRichwala@users.noreply.github.com>
2021-04-20 17:26:46 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,210 @@
+var PLAY = 1;
+var END = 0;
+var gameState = PLAY;
+
+var steve, steve_running, steve_collided;
+var ground, invisibleGround, groundImage;
+var coinGroup,coinImage;
+
+var cloudsGroup, cloudImage;
+var obstaclesGroup, obstacle1, obstacle2;
+
+var score=0;
+
+var gameOver, restart;
+
+localStorage["HighestScore"] = 0;
+
+function preload(){
+  steve_running =   loadAnimation("steve1.png","steve2.png");
+  steve_collided = loadAnimation("steve_collided.png");
+  
+  groundImage = loadImage("ground.png");
+  
+  cloudImage = loadImage("cloud.png");
+  
+  obstacle1 = loadImage("obstacle1.png");
+  obstacle2 = loadImage("obstacle2.png");
+  coinImage = loadImage("coin.png");
+  
+  gameOverImg = loadImage("gameOver.png");
+  restartImg = loadImage("restart.png");
+}
+
+function setup() {
+  createCanvas(600, 200);
+  
+  steve= createSprite(50,180,20,50);
+  
+  steve.addAnimation("running", steve_running);
+  steve.addAnimation("collided", steve_collided);
+  steve.scale = 0.5;
+  
+  ground = createSprite(200,180,400,20);
+  ground.addImage("ground",groundImage);
+  ground.x = ground.width /2;
+  ground.velocityX = -(6 + 3*score/100);
+  
+  gameOver = createSprite(300,100);
+  gameOver.addImage(gameOverImg);
+  
+  restart = createSprite(300,140);
+  restart.addImage(restartImg);
+  
+  gameOver.scale = 0.5;
+  restart.scale = 0.5;
+
+  gameOver.visible = false;
+  restart.visible = false;
+  
+  invisibleGround = createSprite(200,190,400,10);
+  invisibleGround.visible = false;
+  
+  cloudsGroup = new Group();
+  obstaclesGroup = new Group();
+  coinGroup=new Group();
+  
+  score = 0;
+}
+
+function draw() {
+  //trex.debug = true;
+  background(255);
+  text("Score: "+ score, 500,50);
+  
+  if (gameState===PLAY){
+    score = score + Math.round(getFrameRate()/60);
+    ground.velocityX = -(6 + 3*score/100);
+  
+    if(keyDown("space") && steve.y >= 159) {
+      steve.velocityY = -12;
+    }
+  
+    steve.velocityY = steve.velocityY + 0.8
+  
+    if (ground.x < 0){
+      ground.x = ground.width/2;
+    }
+  
+    trex.collide(invisibleGround);
+    spawnClouds();
+    spawnObstacles();
+    spawncoin();
+  
+    if(obstaclesGroup.isTouching(steve)){
+        gameState = END;
+    }
+  }
+  else if (gameState === END) {
+    gameOver.visible = true;
+    restart.visible = true;
+    
+    //set velcity of each game object to 0
+    ground.velocityX = 0;
+    steve.velocityY = 0;
+    obstaclesGroup.setVelocityXEach(0);
+    cloudsGroup.setVelocityXEach(0);
+    coinGroup.setVelocityXEach(0);
+    
+    //change the trex animation
+    steve.changeAnimation("collided",steve_collided);
+    
+    //set lifetime of the game objects so that they are never destroyed
+    obstaclesGroup.setLifetimeEach(-1);
+    cloudsGroup.setLifetimeEach(-1);
+    
+    if(mousePressedOver(restart)) {
+      reset();
+    }
+  }
+  if(coinGroup.isTouching(Steve)){
+    coin.visible = false;
+  }
+  
+  drawSprites();
+}
+
+function spawnClouds() {
+  //write code here to spawn the clouds
+  if (frameCount % 60 === 0) {
+    var cloud = createSprite(600,120,40,10);
+    cloud.y = Math.round(random(80,120));
+    cloud.addImage(cloudImage);
+    cloud.scale = 0.5;
+    cloud.velocityX = -3;
+    
+     //assign lifetime to the variable
+    cloud.lifetime = 200;
+    
+    //adjust the depth
+    cloud.depth = steve.depth;
+    steve.depth = steve.depth + 1;
+    
+    //add each cloud to the group
+    cloudsGroup.add(cloud);
+  }
+  
+}
+function spawncoin(){
+ if (frameCount % 70 === 0) {
+    var coin = createSprite(600,120,40,10);
+    coin.y = Math.round(random(70,110));
+    coin.addImage(coinImage);
+    coin.velocityX = -3;
+   coin.lifetime = 200;
+   coinGroup.add(coin);
+   coin.scale = 0.3;
+}
+  
+}
+
+function spawnObstacles() {
+  if(frameCount % 60 === 0) {
+    var obstacle = createSprite(600,165,10,40);
+    //obstacle.debug = true;
+    obstacle.velocityX = -(6 + 3*score/100);
+    
+    //generate random obstacles
+    var rand = Math.round(random(1,6));
+    switch(rand) {
+      case 1: obstacle.addImage(obstacle1);
+              break;
+      case 2: obstacle.addImage(obstacle2);
+              break;
+      case 3: obstacle.addImage(obstacle3);
+              break;
+      case 4: obstacle.addImage(obstacle4);
+              break;
+      case 5: obstacle.addImage(obstacle5);
+              break;
+      case 6: obstacle.addImage(obstacle6);
+              break;
+      default: break;
+    }
+    
+    //assign scale and lifetime to the obstacle           
+    obstacle.scale = 0.5;
+    obstacle.lifetime = 300;
+    //add each obstacle to the group
+    obstaclesGroup.add(obstacle);
+  }
+}
+
+function reset(){
+  gameState = PLAY;
+  gameOver.visible = false;
+  restart.visible = false;
+  
+  obstaclesGroup.destroyEach();
+  cloudsGroup.destroyEach();
+  
+  steve.changeAnimation("running",steve_running);
+  
+  if(localStorage["HighestScore"]<score){
+    localStorage["HighestScore"] = score;
+  }
+  console.log(localStorage["HighestScore"]);
+  
+  score = 0;
+  
+}
